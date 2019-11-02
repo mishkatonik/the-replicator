b24bebb65bd45db33c78f2e3dd29f85c43aa7b1e
Mish <mishell.mercer@gmail.com>
2019-11-01 17:51:49 -0700

    created script and separated pages, ready to execute

---
+++ build.sh
@@ -0,0 +1,3 @@
+cat ./templates/top.html ./content/index.html ./templates/bottom.html
+cat ./templates/top.html ./content/about.html ./templates/bottom.html
+cat ./templates/top.html ./content/meet-the-chef.html ./templates/bottom.html
