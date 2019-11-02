13ec460783da6579c2cff98e93fa95e66dc05905
Mish <mishell.mercer@gmail.com>
2019-11-01 17:55:11 -0700

    changed css directory so it should work

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
-cat ./templates/top.html ./content/index.html ./templates/bottom.html
-cat ./templates/top.html ./content/about.html ./templates/bottom.html
-cat ./templates/top.html ./content/meet-the-chef.html ./templates/bottom.html
+cat ./templates/top.html ./content/index.html ./templates/bottom.html > ./docs/index.html
+cat ./templates/top.html ./content/about.html ./templates/bottom.html > ./docs/about.html
+cat ./templates/top.html ./content/meet-the-chef.html ./templates/bottom.html > ./docs/meet-the-chef.html
