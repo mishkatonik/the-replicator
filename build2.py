4f0d7bd2989daab2e715af6a9e9c3fd1785e0747
Mish <mishell.mercer@gmail.com>
2019-11-07 23:20:32 -0800

    created new build2, added pages list and copy-pasted previous builder

---
+++ build2.py
@@ -0,0 +1,49 @@
+def main():
+	# templates
+	top = open('templates/top.html').read()
+	bottom = open('templates/bottom.html').read()
+
+	# index
+	content = open('content/index.html').read()
+
+	index_html = top + content + bottom
+	open('docs/index.html', 'w+').write(index_html)
+
+	# about
+	content = open('content/about.html').read()
+
+	about_html = top + content + bottom
+	open('docs/about.html', 'w+').write(about_html)
+
+	# meet-the-chef
+	content = open('content/meet-the-chef.html').read()
+
+	meet_the_chef_html = top + content + bottom
+	open('docs/meet-the-chef.html', 'w+').write(meet_the_chef_html)
+
+pages = [
+	{
+	'filename': 'content/index.html',
+	'output': 'docs/index.html',
+	'title': 'The Replicator'
+	},
+	{
+	'filename': 'content/about.html',
+	'output': 'docs/about.html',
+	'title': 'About - The Food of Star Trek'
+	},
+	{
+	'filename': 'content/meet-the-chef.html',
+	'output': 'docs/meet-the-chef.html',
+	'title': 'Meet the Chef'
+	}
+]
+
+
+
+
+
+
+
+if __name__ == "__main__":
+	main()
