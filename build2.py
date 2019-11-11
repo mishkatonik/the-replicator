0d19e79bd704772579c4b813a14569f97f987cc6
Mish <mishell.mercer@gmail.com>
2019-11-10 16:04:18 -0800

    script runs but prints the same content to all pages whyyyyy

--- build2.py
+++ build2.py
@@ -2,12 +2,12 @@ pages = [
 	{
 	'filename': 'content/index.html',
 	'output': 'docs/index.html',
-	'title': 'What\'s Cookin',
+	'title': 'The Replicator',
 	},
 	{
 	'filename': 'content/about.html',
 	'output': 'docs/about.html',
-	'title': '<h2>ABOUT The Food of <em>Star Trek</em></h2>',
+	'title': 'About - The Food of Star Trek',
 	},
 	{
 	'filename': 'content/meet-the-chef.html',
@@ -16,10 +16,10 @@ pages = [
 	}
 ]
 
+
 # Read in base template file
 def get_template():
-	for page in pages:
-		base_template = open('templates/base.html').read()
+	base_template = open('templates/base.html').read()
 	return base_template
 
 # Read in content files
@@ -29,19 +29,22 @@ def get_content():
 	return content
 
 # Compile the fullpage, which represents the fully rendered html webpage
-def compile():
+def compile(base_template, content):
 	for page in pages:
-		fullpage = base_template.replace('{{content}}', content) + base_template.replace('{{title}}', page['title'])
+		fullpage = base_template.replace('{{content}}', content) # + base_template.replace('{{title}}', page['title'])
 		open(page['output'], "w+").write(fullpage)
-
+	# return fullpage
 
 # Put em all together
 def main():
-	get_template()
-	get_content()
-	compile()
+	base_template = get_template()
+	content = get_content()
+	compile(base_template, content)
 
-main()
+for page in pages:
+	main()
+	# get_template()
+	# get_content()
 
 if __name__ == "__main__":
 	main()
