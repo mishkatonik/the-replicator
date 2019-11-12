e5006228f82a5e9c8e1851af2659bb8af61ff67e
Mish <mishell.mercer@gmail.com>
2019-11-11 23:50:13 -0800

    defined active_buttons function as an if-else, might want a better solution but at least this one works

--- build2.py
+++ build2.py
@@ -3,16 +3,19 @@ pages = [
 	'filename': 'content/index.html',
 	'output': 'docs/index.html',
 	'title': 'The Replicator',
+	# 'is_active': '{{recipes_active}}',
 	},
 	{
 	'filename': 'content/about.html',
 	'output': 'docs/about.html',
 	'title': 'About - The Food of Star Trek',
+	# 'is_active': '{{about_active}}',
 	},
 	{
 	'filename': 'content/meet-the-chef.html',
 	'output': 'docs/meet-the-chef.html',
 	'title': 'Meet the Chef',
+	# 'is_active': '{{meet_the_chef_active}}',
 	}
 ]
 
@@ -32,21 +35,26 @@ def compile(page, base_template, content):
 	fullpage = base_template.replace('{{content}}', content).replace('{{title}}', page['title'])
 	return fullpage
 
+# Indicate which page is active and set the corresponding button to active class
+	# there's gotta be a better way to do this...
+def active_buttons(page, fullpage):
+	if page['output'] == 'docs/index.html': 
+		actvive_page = fullpage.replace('{{recipes_active}}', 'active')
+	elif page['output'] == 'docs/about.html': 
+		actvive_page = fullpage.replace('{{about_active}}', 'active')
+	elif page['output'] == 'docs/meet-the-chef.html': 
+		actvive_page = fullpage.replace('{{meet_the_chef_active}}', 'active')
+	return actvive_page
+
+
 # Put em all together
 def main():
 	for page in pages:
 		base_template = get_template()
 		content = get_content(page)
 		fullpage = compile(page, base_template, content)
-		open(page['output'], "w+").write(fullpage)
+		active_page = active_buttons(page, fullpage)
+		open(page['output'], "w+").write(active_page)
 
-
-# for page in pages:
-main()
-	
-
-	# get_template()
-	# get_content()
-
-#if __name__ == "__main__":
-#	main()
+if __name__ == "__main__":
+	main()
