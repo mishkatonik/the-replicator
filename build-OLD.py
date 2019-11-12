a8da1756db266170cd974c4d43d9475e279b219d
Mish <mishell.mercer@gmail.com>
2019-11-11 23:57:08 -0800

    deleted bash file and renamed old build file. updated readme

---
+++ build-OLD.py
@@ -0,0 +1,21 @@
+# templates
+top = open('templates/top.html').read()
+bottom = open('templates/bottom.html').read()
+
+# index
+content = open('content/index.html').read()
+
+index_html = top + content + bottom
+open('docs/index.html', 'w+').write(index_html)
+
+# about
+content = open('content/about.html').read()
+
+about_html = top + content + bottom
+open('docs/about.html', 'w+').write(about_html)
+
+# meet-the-chef
+content = open('content/meet-the-chef.html').read()
+
+meet_the_chef_html = top + content + bottom
+open('docs/meet-the-chef.html', 'w+').write(meet_the_chef_html)
--- build.py
+++ build.py
@@ -1,21 +1,60 @@
-# templates
-top = open('templates/top.html').read()
-bottom = open('templates/bottom.html').read()
+pages = [
+	{
+	'filename': 'content/index.html',
+	'output': 'docs/index.html',
+	'title': 'The Replicator',
+	# 'is_active': '{{recipes_active}}',
+	},
+	{
+	'filename': 'content/about.html',
+	'output': 'docs/about.html',
+	'title': 'About - The Food of Star Trek',
+	# 'is_active': '{{about_active}}',
+	},
+	{
+	'filename': 'content/meet-the-chef.html',
+	'output': 'docs/meet-the-chef.html',
+	'title': 'Meet the Chef',
+	# 'is_active': '{{meet_the_chef_active}}',
+	}
+]
 
-# index
-content = open('content/index.html').read()
 
-index_html = top + content + bottom
-open('docs/index.html', 'w+').write(index_html)
+# Read in base template file
+def get_template():
+	base_template = open('templates/base.html').read()
+	return base_template
 
-# about
-content = open('content/about.html').read()
+# Read in content files
+def get_content(page):
+	content = open(page['filename']).read()
+	return content
 
-about_html = top + content + bottom
-open('docs/about.html', 'w+').write(about_html)
+# Compile the fullpage, which represents the fully rendered html webpage
+def compile(page, base_template, content):
+	fullpage = base_template.replace('{{content}}', content).replace('{{title}}', page['title'])
+	return fullpage
 
-# meet-the-chef
-content = open('content/meet-the-chef.html').read()
+# Indicate which page is active and set the corresponding button to active class
+	# there's gotta be a better way to do this...
+def active_buttons(page, fullpage):
+	if page['output'] == 'docs/index.html': 
+		actvive_page = fullpage.replace('{{recipes_active}}', 'active')
+	elif page['output'] == 'docs/about.html': 
+		actvive_page = fullpage.replace('{{about_active}}', 'active')
+	elif page['output'] == 'docs/meet-the-chef.html': 
+		actvive_page = fullpage.replace('{{meet_the_chef_active}}', 'active')
+	return actvive_page
 
-meet_the_chef_html = top + content + bottom
-open('docs/meet-the-chef.html', 'w+').write(meet_the_chef_html)
+
+# Put em all together
+def main():
+	for page in pages:
+		base_template = get_template()
+		content = get_content(page)
+		fullpage = compile(page, base_template, content)
+		active_page = active_buttons(page, fullpage)
+		open(page['output'], "w+").write(active_page)
+
+if __name__ == "__main__":
+	main()
